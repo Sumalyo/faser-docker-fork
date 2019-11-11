d29638f9694fb604fb685aa6314993950348864e
Samuel Meehan <smeehan12@gmail.com>
2019-11-11 19:02:00 +0100

    added all those pesky root libraries

--- startup.sh
+++ startup.sh
@@ -3,5 +3,5 @@ source /opt/lcg/gcc/8.3.0/x86_64-centos7/setup.sh
 
 echo "ENDING Faser Env Setup"
 
-echo "Placing you in bash env"
+echo "Placing you in bash env - Remember, YOU ARE A BEAUTIFUL PERSON! Now go get some!"
 /bin/bash
