85f72670031d6a240402f238c3da7eb9ba2c1912
Sumalyo <datudutta@gmail.com>
2023-07-23 16:30:41 -0400

    changes in file to include from source

--- faser-daq/startup.sh
+++ faser-daq/startup.sh
@@ -8,5 +8,7 @@ echo "Now starting supervisord"
 /usr/local/bin/supervisord -c /etc/supervisor/supervisord.conf
 echo "Now starting redis database"
 /usr/bin/redis-server /etc/redis.conf --daemonize yes
+# sed -i 's/\(moody-incl arch=linux-None-x86_64\)/\1, zstd arch=linux-centos7-x86_64, googletest arch=linux-centos7-x86_64, lz4@1.9.2 arch=linux-centos7-x86_64, brotli arch=linux-centos7-x86_64/' /daqling-spack-repo/spack/var/spack/environments/daqling/spack.yaml;
+# . /daqling-spack-repo/spack/share/spack/setup-env.sh ; spack env activate daqling ; spack find; spack install ;
 ps aux
 /bin/bash
